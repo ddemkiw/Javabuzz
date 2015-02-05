293e896db3c21d238fa6c354583ee17500ba5d1c
Danielle Demkiw <ddemkiw@gmail.com>
2015-02-05 20:39:01 +0000

    adding tests

--- js/Javabuzz.js
+++ js/Javabuzz.js
@@ -1,27 +1,24 @@
-var Javabuzz = function () {};
+ var Javabuzz = function() {}; 
 
-Javabuzz.prototype.isDivisibleByThree = function(number) {
- return this._isDivisibleBy(number, 3);
+ Javabuzz.prototype.isDivisibleByThree = function(number) {
+   return this.isDivisibleBy(number, 3); 
+ };
+
+ Javabuzz.prototype.isDivisibleByFive = function(number) {
+   return this.isDivisibleBy(number, 5);
+ };
+
+ Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+   return this.isDivisibleBy(number, 15);
+ };
+
+Javabuzz.prototype.isDivisibleBy = function(number, divisor) {
+    return (number % divisor === 0);
 };
 
-Javabuzz.prototype.isDivisibleByFive = function(number) {
-  return this._isDivisibleBy(number, 5);
-};
-
-Javabuzz.prototype.isDivisibleByFifteen = function(number) {
-  return this._isDivisibleBy(number, 15);
-};
-
-Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
-  return (number % divisor === 0);
-};
-
-Javabuzz.prototype.says = function(number) {
-  if (this.isDivisibleByFifteen(number)) return "Javabuzz";
-  if (this.isDivisibleByFive(number)) return "Buzz";
-  if (this.isDivisibleByThree(number)) return "Java";
+Javabuzz.prototype.play = function(number) {
+  if (this.isDivisibleByFifteen(number)) return 'Javabuzz';
+  if (this.isDivisibleByFive(number)) return 'Buzz';
+  if (this.isDivisibleByThree(number)) return 'Java';  
   return number;
 };
-
-
-
--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,72 +1,54 @@
-describe('Javabuzz',function() {
-  var javabuzz;
+describe('Javabuzz', function(){
+  
+  beforeEach(function(){
+    javabuzz = new Javabuzz;
+  });
 
-// beforeEach(function() {
-//     javabuzz = new Javabuzz();
-//   });
+  describe('knows when a number is', function(){
 
-  describe('knows when a number is',function(){
-
-    it('divisible by 3',function(){
+    it('divisible by 3', function(){
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
-  });
 
-  describe('knows when a number is NOT', function(){
-    
-    it('divisible by three', function(){
+    it('NOT divisible by 3', function(){
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
     });
-  });
-  describe('knows when a number is', function(){
-    
-    it('divisible by 5', function(){
+
+    it('divisible by 5', function() {
       expect(javabuzz.isDivisibleByFive(5)).toBe(true);
     });
-  });
 
-  describe('knows when a number is NOT', function(){
-    
-    it('divisible by 5', function(){
+    it('NOT divisible by 5', function(){
       expect(javabuzz.isDivisibleByFive(1)).toBe(false);
     });
-  });
 
-  describe('knows when a number is', function(){
-    
-    it('divisible by 15', function(){
+    it('divisible  by 15', function(){
       expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
     });
-  });
 
-  describe('knows when a number is NOT',function(){
-    
-    it('divisible by 15', function(){
+    it('NOT divisible by 15', function(){
       expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
     });
   });
 
-  describe('when playing, says', function(){
-    
-    it('"Java" when a number is divisible by 3', function(){
-      expect(javabuzz.says(3)).toEqual("Java");  
+  describe('returns', function(){
+
+    it('Java when a number is divisible by 3', function(){
+      expect(javabuzz.play(3)).toEqual('Java');
     });
 
-    it('"Buzz" when a number is divisible by 5', function(){
-      expect(javabuzz.says(5)).toEqual("Buzz");  
+    it('Buzz when a number is divisible  by 5', function(){
+      expect(javabuzz.play(5)).toEqual('Buzz')
     });
 
-    it('"Javabuzz" when a number is divisible by 15', function(){
-      expect(javabuzz.says(15)).toEqual("Javabuzz");  
+    it('Javabuzz when a number is divisible by 15', function(){
+      expect(javabuzz.play(15)).toEqual('Javabuzz');
     });
 
-    it('"orignal number" when number is NOT divisible by 3,5 or 15', function(){
-      expect(javabuzz.says(2)).toEqual(2)
+    it('number if not divisible by 3,5 or 15',function(){
+      expect(javabuzz.play(1)).toEqual(1)
     });
+   
   });
-})
 
-
-
-
-
+});
